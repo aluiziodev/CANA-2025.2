afacb7f3d7b18fc1c8a112ef8b8209a905c19d75
aluizioneto <aluneto@alu.ufc.br>
2025-11-15 13:05:00 -0300

    confirm

---
+++ ORDENAÇAO/quickSort.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printVetor(int V[], int n){
+    for (int i = 0; i < n; i++)
+        printf("%d ", V[i]);
+    cout << endl;   
+}
+
+
+int particiona(int V[], int i, int f, int pivo){
+    int it = i-1;
+    int j = i;
+    while(j<=f){
+        if(V[j]<=pivo){
+            it++;
+            swap(V[it], V[j]);
+        }
+        j++;
+    }
+    return it;
+}
+
+
+void quickSort(int V[], int i, int f){
+    if(i<f){
+        int pivot = V[f];
+        int p = particiona(V, i, f, pivot);
+        quickSort(V, i, p-1);
+        quickSort(V, p+1, f);
+    }
+}
+
+
+int main(){
+    
+    int V[] = {7, 6, 2, 4, 1};
+    quickSort(V, 0, 4);
+    printVetor (V, 5);
+    
+    
+}
