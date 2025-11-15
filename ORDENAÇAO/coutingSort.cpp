330138f68793895f466fa96b78e7ef18eaaf200a
aluiziodev <aluneto2000@gmail.com>
2025-11-15 14:03:49 -0300

    confirm

---
+++ ORDENAÇAO/coutingSort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printVetor(int V[], int n){
+    for (int i = 0; i < n; i++)
+        printf("%d ", V[i]);
+    cout << endl;   
+}
+
+
+
+void countingSort(int V[], int n, int k){
+    int C[k+1];
+    int B[n];
+    for(int i=0;i<k+1;i++) C[i] = 0;
+    for(int i=0;i<n;i++) B[i] = 0;
+    for(int i=0;i<n;i++) C[V[i]]++;
+    for(int i=1;i<k+1;i++) C[i] += C[i-1];
+    for(int i=n-1; i>=0;i--){
+        C[V[i]]--;
+        B[C[V[i]]] = V[i];
+    }
+    for(int i=0;i<n;i++) V[i] = B[i];
+
+}
+
+
+int main(){
+    
+    int V[] = {7, 6, 2, 4, 1};
+    countingSort(V, 5, 7);
+    printVetor (V, 5);
+    
+    
+}
