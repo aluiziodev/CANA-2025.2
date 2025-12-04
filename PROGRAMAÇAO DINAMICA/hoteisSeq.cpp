ca1f2e9cefa8384f047da3cc31d43251b05fe12e
aluiziodev <aluneto2000@gmail.com>
2025-12-04 18:07:23 -0300

    Inclusao do algoritmo de seqHoteis

---
+++ PROGRAMAÇAO DINAMICA/hoteisSeq.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef vector<int> vi;
+
+vi hoteisSeq(int V[], int n, int k){
+    vector<vi> seqHoteis(n);
+    int memo[n];
+    memo[0] = 0;
+    seqHoteis[0]= {0};
+
+    for(int i =1;i<n;i++){
+        vi cam = seqHoteis[0];
+        int min = memo[0]+pow(200-(V[i]-V[0]), 2);
+        for(int j =1;j<i;j++){
+            int custo = memo[j]+pow(200-(V[i]-V[j]), 2);
+            if(custo<min){
+                min = custo;
+                cam = seqHoteis[j];
+            }
+        } 
+        memo[i] = min;
+        cam.push_back(i);
+        seqHoteis[i] = cam;
+    }
+    return seqHoteis[k];
+    
+}
+
+int main(){
+    int V[] = {0, 150, 350, 480, 600};
+    int n = 5;
+    int k = 4;
+    vi seqHoteis = hoteisSeq(V, n, k);
+    cout << "Hoteis a parar: ";
+    for(int i =0;i< (int)seqHoteis.size();i++){
+        cout << seqHoteis[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
